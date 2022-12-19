22042394aeade19a9754c37ac159fab388003832
afanbagwan <afanbagwan>
2022-12-19 13:01:47 +0530

    Provider updates

--- ec2-resource.tf
+++ ec2-resource.tf
@@ -12,15 +12,13 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
 # Resource Block
 resource "aws_instance" "web_server" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  user_data = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "Nova-WebServer"
   }
